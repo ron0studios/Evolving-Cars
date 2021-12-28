8d4c50c9618f64fa2a2e78a20b4ac4d9f0903696
Ron0Studios <rudrrayan@gmail.com>
2021-12-28 15:36:02 +0000

    wheel constantly works
    the wheel now moves continuously

---
+++ wheel.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export(int,0,20,0.5) var turningspeed = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	angular_velocity = turningspeed
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	angular_velocity = turningspeed
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
