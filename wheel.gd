bfe0a16e1e0675898307b525d6b62fbe7462bc49
Coloursplash <darealredshift@gmail.com>
2021-12-31 09:21:24 +0000

    FIXED ALL YOUR MISTAKES WTF DO YOU EVEN RUN THIS

--- wheel.gd
+++ wheel.gd
@@ -1,18 +1,11 @@
 extends RigidBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export(int,0,20,0.5) var turningspeed = 10
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	angular_velocity = turningspeed
-	pass # Replace with function body.
 
 func _physics_process(_delta):
 	angular_velocity = turningspeed
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
